5986f391919989f91e4646f0eccd22ae8177443d
Jakub Donic <donicjakub@gmail.com>
2024-04-28 16:57:23 +0200

    user authentication

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,10 +5,10 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ("age",)
+        fields = ("age", "username", "email")
 
 
 class CustomUserChangeForm(UserChangeForm): 
     class Meta:
         model = CustomUser
-        fields = UserChangeForm.Meta.fields
+        fields = ("age", "username", "email")
